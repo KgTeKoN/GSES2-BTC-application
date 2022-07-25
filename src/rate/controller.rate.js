f51ad0a705e21391a03fede11847d2c62ca5c351
Андрій Петруняк <hasherama8@gmail.com>
2022-07-25 10:22:19 +0300

    finished first stage

--- src/rate/controller.rate.js
+++ src/rate/controller.rate.js
@@ -1,10 +1,13 @@
 const { getRate } = require("./handlers.rate.js");
 
 const controllerGetRate = async (req, res) => {
-    const rate = await getRate();
-    console.log(rate);
-    res.status(rate.code).send(`Bitcoin price: ${rate.data.bitcoin.uah} UAH`);
-    res.end();
+    try {
+        const rate = await getRate();
+        res.status(rate.code).send(`Bitcoin price: ${rate.data.bitcoin.uah} UAH`);
+        res.end();
+    } catch (e) {
+        res.status(400).send('Invalid status value');
+    }
 }
 
 module.exports = { controllerGetRate };
