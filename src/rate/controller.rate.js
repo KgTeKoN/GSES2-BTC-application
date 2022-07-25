1a738bb9776e8d2cc7568518e61374d89985bbf6
Андрій Петруняк <hasherama8@gmail.com>
2022-07-26 00:05:57 +0300

    finished work try catch for first stage

--- src/rate/controller.rate.js
+++ src/rate/controller.rate.js
@@ -1,13 +1,9 @@
 const { getRate } = require("./handlers.rate.js");
 
 const controllerGetRate = async (req, res) => {
-    try {
         const rate = await getRate();
-        res.status(rate.code).send(`Bitcoin price: ${rate.data.bitcoin.uah} UAH`);
+        res.status(rate.status).send(rate.message);
         res.end();
-    } catch (e) {
-        res.status(400).send('Invalid status value');
-    }
 }
 
 module.exports = { controllerGetRate };
--- src/rate/thirdAPI.rate.js
+++ src/rate/thirdAPI.rate.js
@@ -2,10 +2,15 @@ const CoinGecko = require('coingecko-api');
 
 const CoinGeckoClient = new CoinGecko();
 
-let readRate = async() =>
-    await CoinGeckoClient.simple.price({
-        ids: ['bitcoin'],
-        vs_currencies: ['uah'],
-    });
-
+let readRate = async() => {
+    try {
+        let data = await CoinGeckoClient.simple.price({
+                ids: ['bitcoin'],
+                vs_currencies: ['uah'],
+        });
+        return {status: data.code, message: `Bitcoin price: ${data.data.bitcoin.uah} UAH`}
+    } catch(e) {
+        return {status: 400, message: 'Invalid status value'}
+    }
+}
 module.exports = { readRate };
