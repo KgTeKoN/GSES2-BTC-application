505ace098b69377b770f929ecb8fbbbadb7a9caf
Андрій Петруняк <hasherama8@gmail.com>
2022-07-27 01:13:04 +0300

    before joi

--- src/emails/subscription/handlers.subscription.js
+++ src/emails/subscription/handlers.subscription.js
@@ -4,7 +4,7 @@ const { writeFile } = require('./write.file.js')
 const handlersSubscriber = async (email) => {
     const isExist = await validateExistEmail(email);
     if (isExist) { return { status: 409, info: 'E-mail вже є в базі даних'} };
-    writeFile(email);
+    await writeFile(email);
     return { status: 200, info: 'E-mail додано'}
 }
 
