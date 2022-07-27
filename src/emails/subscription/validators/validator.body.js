9bba7d48e9077eb5391019c97a50d3be285a6094
Андрій Петруняк <hasherama8@gmail.com>
2022-07-27 09:39:08 +0300

    repared validator

--- src/emails/subscription/validators/validator.body.js
+++ src/emails/subscription/validators/validator.body.js
@@ -3,7 +3,7 @@ const { signupSchema } = require('./validate.schema.js');
 const bodyValidator = async (req, res, next) => {
     try {
         await signupSchema.validateAsync(req.body);
-        return true;
+        return next();
     } catch (e) {
         const result = e.details[0].message
         res.status(400).send(`Це не e-mail \n ${result}`);
