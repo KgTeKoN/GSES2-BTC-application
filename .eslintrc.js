ce80244063c1536094f7c9998c3e0c9df6565568
Андрій Петруняк <hasherama8@gmail.com>
2022-07-28 22:45:45 +0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		node: true,
+		browser: false,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: [
+		'xo',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+	},
+	rules: {
+		"linebreak-style": 0,
+		"no-underscore-dangle": "off",
+		"max-len": [
+			"error",
+			125
+		],
+		"no-param-reassign": [
+			"error",
+			{
+				"props": false
+			}
+		],
+		"no-useless-escape": "off"
+	},
+};
